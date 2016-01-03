b87de656fca1e77b760c4a0d4bbee67bb487946c
Christine Newbery <chris@Christines-iMac.local>
2016-01-03 13:28:31 +1000

    basic functionality  - added gulpfile  - added package.json  - added other basic files

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('less', function() {
+  gulp.src('less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['serve']);
+
+gulp.task('serve', ['less'], function() {
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("css/*.css").on('change', browserSync.reload);
+    gulp.watch("index.html").on('change', browserSync.reload);
+    gulp.watch("js/*.js").on('change', browserSync.reload);
+    gulp.watch('less/*.less', ['less']);
+});
